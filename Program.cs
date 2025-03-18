2f40103c1fd18cd16aa6420e3e9ecbd6191c1e41
Nmihal1 <nmihal28@gmail.com>
2025-03-18 17:30:37 +0200

    Added Product & Dot functionalities

--- Program.cs
+++ Program.cs
@@ -5,10 +5,18 @@ public class Program {
         Vector2D vectorA = new Vector2D(5, 8);
         Vector2D vectorB = new Vector2D(16, 13);
         Vector2D sum = vectorA + vectorB;
+        Vector2D sub = vectorB - vectorA;
+        double dot = Vector2D.Dot(vectorA, vectorB);
+        Vector2D prod = vectorA * 3;
 
-        Console.WriteLine(vectorA);
-        Console.WriteLine(vectorB);
-        Console.WriteLine(sum);
-        Console.WriteLine("Sum Length: " + sum.Length);
+        Console.WriteLine("A: " + vectorA);
+        Console.WriteLine("B: " + vectorB);
+        Console.WriteLine("A - B: " + sub);
+        Console.WriteLine("Dot(A,B): " + dot);
+        Console.WriteLine("Length A: " + vectorA.Length);
+        Console.WriteLine("Length B: " + vectorB.Length);
+        Console.WriteLine("Prod A * 3: " + prod);
+        Console.WriteLine("Unit A: " + vectorA.Unit);
+        Console.WriteLine("Length Unit A: " + vectorA.Unit.Length);
     }
 }
--- Vector2D.cs
+++ Vector2D.cs
@@ -1,28 +1,38 @@
-﻿using System;
+﻿namespace vectors {
+    public struct Vector2D {
 
-namespace vectors
-{
-    public struct Vector2D
-    {
         public double X { get; private set; }
         public double Y { get; private set; }
+
+        public Vector2D Unit {
+            get {
+                double length = this.SqrMagnitude;
+                return new Vector2D(X / Length, Y / Length);
+            }
+        }
 
         public double SqrMagnitude => SquareMagnitude(this);
         public double Length => Magnitude(this);
 
-        public Vector2D(double x, double y)
-        {
+        public Vector2D(double x, double y) {
             X = x;
             Y = y;
         }
 
-        public static Vector2D Add(Vector2D v1, Vector2D v2)
-        {
+        public static Vector2D Add(Vector2D v1, Vector2D v2) {
             return new Vector2D(v1.X + v2.X, v1.Y + v2.Y);
         }
-        
+
         public static Vector2D Subtract(Vector2D v1, Vector2D v2) {
-            return new Vector2D(v2.X - v1.X, v2.Y - v1.Y);
+            return new Vector2D(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static double Dot(Vector2D v1, Vector2D v2) {
+            return (v1.X * v2.X + v1.Y * v2.Y);
+        }
+
+        public static Vector2D Product(Vector2D v, double d) {
+            return new Vector2D(v.X * d, v.Y * d);
         }
 
         public static double SquareMagnitude(Vector2D v1) {
@@ -42,6 +52,10 @@ namespace vectors
 
         public static Vector2D operator -(Vector2D v1, Vector2D v2) {
             return Subtract(v1, v2);
+        }
+
+        public static Vector2D operator *(Vector2D v, double d) {
+            return Product(v, d);
         }
 
         public override string ToString() {
