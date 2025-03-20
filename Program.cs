c0c1ef4b1e2b7b17714f0f09f9c3f3c40d0802e5
Michael-Evangelos Diamantis <michaeldiamantis17@gmail.com>
2025-03-20 18:08:36 +0200

    Lerping and Extrapolating

--- Program.cs
+++ Program.cs
@@ -1,22 +1,65 @@
 ﻿using vectors;
 
 public class Program {
-    static void Main(string[] args) {
-        Vector2D vectorA = new Vector2D(5, 8);
-        Vector2D vectorB = new Vector2D(16, 13);
+    static void Main(string[] _args) {
+        /* Vector2D vectorA = new Vector2D(5, 8);
+        Vector2D vectorB = new Vector2D(16, 0);
         Vector2D sum = vectorA + vectorB;
         Vector2D sub = vectorB - vectorA;
-        double dot = Vector2D.Dot(vectorA, vectorB);
         Vector2D prod = vectorA * 3;
+        Vector2D j = new Vector2D(0, 4);
 
         Console.WriteLine("A: " + vectorA);
         Console.WriteLine("B: " + vectorB);
         Console.WriteLine("A - B: " + sub);
-        Console.WriteLine("Dot(A,B): " + dot);
         Console.WriteLine("Length A: " + vectorA.Length);
         Console.WriteLine("Length B: " + vectorB.Length);
         Console.WriteLine("Prod A * 3: " + prod);
         Console.WriteLine("Unit A: " + vectorA.Unit);
         Console.WriteLine("Length Unit A: " + vectorA.Unit.Length);
+        Console.WriteLine("J: " + j);
+        Console.WriteLine("Unit J, B: " + Vector2D.Dot(j.Unit, vectorB)); */
+
+        /* double a = 3;
+        double b = 69;
+        double step = 0.1;
+
+        double t = 0;
+        while (t <= 1) {
+            Console.WriteLine($"T {t:0.00}: " + Vector2D.Lerp(a, b, t));
+            t += step;
+        } */
+
+        /* Vector2D a = new Vector2D(0, 0);
+        Vector2D b = new Vector2D(6, 9);
+        double step = 0.1;
+        double t = 0;
+        while (t <= 1) {
+            Console.WriteLine($"T {t:0.00}: " + Vector2D.Lerp(a, b, t));
+            t += step;
+        } */
+
+        /* double speed = 2;
+        double time = 10;
+        Vector2D start = new Vector2D(1, -1);
+        Vector2D end = start * speed * time;
+        Vector2D velocity = start * speed;
+        Console.WriteLine("Start: " + start);
+        Console.WriteLine("End: " + end);
+        Console.WriteLine("Velocity: " + velocity);
+
+        Console.WriteLine("Extrapolation: " + Vector2D.Lerp(start, velocity, 19)); */
+
+        double speed = 2;
+        double time = 10;
+        Vector2D start = new Vector2D(-3, -6);
+        Vector2D end = new Vector2D(-2, -6);
+        Vector2D endNew = start + (end.Unit * speed * time);
+        /*
+        Vector2 endNew = start.transform.position + (start.transform.forward * speed * time);
+        */
+        Console.WriteLine("Start: " + start);
+        Console.WriteLine("End: " + end);
+        Console.WriteLine("End New: " + endNew);
     }
 }
--- Vector2D.cs
+++ Vector2D.cs
@@ -14,48 +14,56 @@
         public double SqrMagnitude => SquareMagnitude(this);
         public double Length => Magnitude(this);
 
-        public Vector2D(double x, double y) {
-            X = x;
-            Y = y;
+        public Vector2D(double _x, double _y) {
+            X = _x;
+            Y = _y;
         }
 
-        public static Vector2D Add(Vector2D v1, Vector2D v2) {
-            return new Vector2D(v1.X + v2.X, v1.Y + v2.Y);
+        public static Vector2D Add(Vector2D _v1, Vector2D _v2) {
+            return new Vector2D(_v1.X + _v2.X, _v1.Y + _v2.Y);
         }
 
-        public static Vector2D Subtract(Vector2D v1, Vector2D v2) {
-            return new Vector2D(v1.X - v2.X, v1.Y - v2.Y);
+        public static Vector2D Subtract(Vector2D _v1, Vector2D _v2) {
+            return new Vector2D(_v1.X - _v2.X, _v1.Y - _v2.Y);
         }
 
-        public static double Dot(Vector2D v1, Vector2D v2) {
-            return (v1.X * v2.X + v1.Y * v2.Y);
+        public static double Dot(Vector2D _v1, Vector2D _v2) {
+            return (_v1.X * _v2.X) + (_v1.Y * _v2.Y);
         }
 
-        public static Vector2D Product(Vector2D v, double d) {
-            return new Vector2D(v.X * d, v.Y * d);
+        public static Vector2D Product(Vector2D _v, double _d) {
+            return new Vector2D(_v.X * _d, _v.Y * _d);
         }
 
-        public static double SquareMagnitude(Vector2D v1) {
-            double squareX = v1.X * v1.X;
-            double squareY = v1.Y * v1.Y;
+        public static double SquareMagnitude(Vector2D _v1) {
+            double squareX = _v1.X * _v1.X;
+            double squareY = _v1.Y * _v1.Y;
 
             return squareX + squareY;
         }
 
-        public static double Magnitude(Vector2D v1) {
-            return Math.Sqrt(SquareMagnitude(v1));
+        public static double Magnitude(Vector2D _v1) {
+            return Math.Sqrt(SquareMagnitude(_v1));
         }
 
-        public static Vector2D operator +(Vector2D v1, Vector2D v2) {
-            return Add(v1, v2);
+        public static double Lerp(double _a, double _b, double _t) {
+            return _a + _t * (_b - _a);
         }
 
-        public static Vector2D operator -(Vector2D v1, Vector2D v2) {
-            return Subtract(v1, v2);
+        public static Vector2D Lerp(Vector2D _v1, Vector2D _v2, double _t) {
+            return new Vector2D(Lerp(_v1.X, _v2.X, _t), Lerp(_v1.Y, _v2.Y, _t));
         }
 
-        public static Vector2D operator *(Vector2D v, double d) {
-            return Product(v, d);
+        public static Vector2D operator +(Vector2D _v1, Vector2D _v2) {
+            return Add(_v1, _v2);
+        }
+
+        public static Vector2D operator -(Vector2D _v1, Vector2D _v2) {
+            return Subtract(_v1, _v2);
+        }
+
+        public static Vector2D operator *(Vector2D _v, double _d) {
+            return Product(_v, _d);
         }
 
         public override string ToString() {
